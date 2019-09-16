2f00a305bd03ad0a483bd021fafcadfc7237b85d
pradeeppathak9 <pradeeppathak9@gmail.com>
2019-09-16 19:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Testing CI/CD with Dockerized Flask using AWS Code Pipeline 2+2=4'
+    return 'Testing CI/CD with Dockerized Flask using AWS Code Pipeline 3+3=6'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
