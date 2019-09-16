53a4b2ff8e6a55ca82678840e8555223ecba273c
pradeeppathak9 <pradeeppathak9@gmail.com>
2019-09-16 16:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Flask Dockerized with AWS CI/CD'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
