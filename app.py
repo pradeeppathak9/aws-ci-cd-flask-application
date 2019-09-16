baf4d2dfc806feb7cad0d7b8ed1e2eab9c5043c0
pradeeppathak9 <pradeeppathak9@gmail.com>
2019-09-16 14:25:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+From flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized with AWS CI/CD'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
